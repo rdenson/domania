274bfdefa9132d4c75f9742a4154b95631a32183
rdenson <richardtdenson@gmail.com>
2019-03-11 17:05:25 -0500

    readying ParseSite(); adding some unit tests

--- parsesitecontent.go
+++ parsesitecontent.go
@@ -14,7 +14,7 @@ import (
 )
 
 
-//enumeration of cipher suites
+//enumeration of cipher suites (from GO crypto/tls)
 var cipherSuitesByCode = map[uint16]string{
   0x0005: "TLS_RSA_WITH_RC4_128_SHA",
   0x000a: "TLS_RSA_WITH_3DES_EDE_CBC_SHA",
@@ -45,7 +45,7 @@ var cipherSuitesByCode = map[uint16]string{
   // that the client is doing version fallback. See RFC 7507.
   0x5600: "TLS_FALLBACK_SCSV",
 }
-//enumeration of ssl/tls versions
+//enumeration of ssl/tls versions (from GO crypto/tls)
 var tlsVersionsByCode = map[uint16]string{
   0x0300: "VersionSSL30",
   0x0301: "VersionTLS10",
@@ -53,7 +53,30 @@ var tlsVersionsByCode = map[uint16]string{
   0x0303: "VersionTLS12",
   0x0304: "VersionTLS13",
 }
+var standardTransport *http.Transport = &http.Transport{
+  DisableCompression: true,
+  IdleConnTimeout: 30 * time.Second,
+  MaxIdleConns: 10,
+}
+var insecureTransport *http.Transport = &http.Transport{
+  DisableCompression: true,
+  IdleConnTimeout: 30 * time.Second,
+  MaxIdleConns: 10,
+  TLSClientConfig: &tls.Config {
+    InsecureSkipVerify: true,
+  },
+}
+var httpClient *http.Client = &http.Client{
+  Transport: standardTransport,
+  Timeout: 30 * time.Second,
+}
 
+/*
+ *  Given an HTTP response, does the request redirect us and does it redirect us
+ *  somewhere secure?
+ *  This function is relying on an http client that intercepts redirects.
+ *  see: http.client -> CheckRedirect()
+*/
 func CheckForRedirection(r *http.Response) (bool, bool, string) {
   var hasHttps bool = false
   var site string = r.Request.URL.String()
@@ -76,61 +99,49 @@ func CheckForRedirection(r *http.Response) (bool, bool, string) {
   return redirectFound, hasHttps, site
 }
 
-func UseStandardClient() *http.Client {
-  tr := &http.Transport{
-    DisableCompression: true,
-    IdleConnTimeout: 30 * time.Second,
-    MaxIdleConns: 10,
+/*
+ *  Make sure our requests have a properly formatted URL. This function can force
+ *  http or https schemes.
+ */
+func FormatUrl(host string, secure bool) string {
+  formattedUrl, err := url.Parse(host)
+  if err != nil || len(host) == 0 {
+    //TODO: better logging
+    //fmt.Printf("FormatUrl() - could not format: %s\n", host)
+    return ""
   }
 
-  client := &http.Client{
-    Transport: tr,
-    Timeout: 30 * time.Second,
-  }
-  client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-    //just return the initial response
-    return http.ErrUseLastResponse
+  if secure {
+    formattedUrl.Scheme = "https"
+  } else {
+    formattedUrl.Scheme = "http"
   }
 
-  return client
+  return formattedUrl.String()
 }
 
-func UseInsecureClient() *http.Client {
-  tr := &http.Transport{
-    DisableCompression: true,
-    IdleConnTimeout: 30 * time.Second,
-    MaxIdleConns: 10,
-    TLSClientConfig: &tls.Config {
-      InsecureSkipVerify: true,
-    },
+func LoadRequest(site string, insecure bool) (*http.Response, error) {
+  //set TLS configuration...
+  if insecure {
+    httpClient.Transport = insecureTransport
+  } else {
+    httpClient.Transport = standardTransport
   }
 
-  client := &http.Client{
-    Transport: tr,
-    Timeout: 30 * time.Second,
-  }
-  client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+  //intercept the request redirect
+  httpClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
     //just return the initial response
     return http.ErrUseLastResponse
-  }
-
-  return client
-}
-
-func LoadRequest(site string, requestClient *http.Client) (*http.Response, error) {
-  if site[0:4] != "http" {
-    site = "http://" + site
-  }
-
-  if requestClient == nil {
-    requestClient = UseStandardClient()
   }
 
   req, _ := http.NewRequest("GET", site, nil)
   req.Close = true
-  return requestClient.Do(req)
+  return httpClient.Do(req)
 }
 
+/*
+ *  Container that holds our results after making a request.
+ */
 type requestResult struct {
   callError error
   certExpiration time.Time
@@ -194,95 +205,36 @@ func (res *requestResult) Serialize() string {
   return jsonString.String()
 }
 
-func main() {
-  //take in some site so we can see what will be returned without recompilation
-  var site string
-  fmt.Println("gimmie a url:")
-  fmt.Scanf("%s", &site)
-  //-----
+/*
+ *  Get security information from an HTTP GET request. Tries to understand the state
+ *  of a site's security by examining redirect behavior and TLS status.
+ */
+func ParseSite(uri string) string {
+  var parseResults *requestResult = new(requestResult)
+  var response *http.Response
+  var requestError error
 
-  rr := new(requestResult)
-  rr.site = site
+  parseResults.site = uri
   //make request to site (should be with scheme: http)
-  resp, respErr := LoadRequest(rr.site, nil)
-  if respErr == nil {
-    //let's look for a redirect; gather some data when we check
-    rr.redirects, rr.redirectsToHttps, rr.site = CheckForRedirection(resp)
-    if !rr.redirectsToHttps {
-      //ok, no redirection... let's try hitting https with the site inputted
-      rr.site = "https://" + site
-    }
-
+  response, requestError = LoadRequest(FormatUrl(parseResults.site, false), false)
+  if requestError == nil {
+    //let's look for a redirect and gather some data while we check
+    parseResults.redirects, parseResults.redirectsToHttps, parseResults.site = CheckForRedirection(response)
     //reload the request, trying to hit https
-    resp, respErr = LoadRequest(rr.site, nil)
+    response, requestError = LoadRequest(FormatUrl(parseResults.site, true), false)
   }
 
-  //error anticipation
-  if respErr != nil && strings.Contains(respErr.Error(), "x509:") {
+  //error anticipation (request could come from one of the calls above)
+  if requestError != nil && strings.Contains(requestError.Error(), "x509:") {
     //error mentions something about the cert, hit it again and don't try to verify the cert
     fmt.Println("retry and skip certificate verification")
-    resp, respErr = LoadRequest(rr.site, UseInsecureClient())
+    response, requestError = LoadRequest(parseResults.site, true)
   }
 
-  //set the last response/error now
-  rr.rawResponse = resp
-  rr.callError = respErr
-  rr.AnalyzeTLS()
-  fmt.Printf("%s\n", rr.Serialize())
-  /*req, _ := http.NewRequest("GET", site, nil)
-  req.Close = true
-  resp, err := client.Do(req)
-  if err != nil {
-    fmt.Printf("!!error!!\n%+v\n", err)
-    os.Exit(1)
-  }
+  //set the last response/error now, we're finished with requests
+  parseResults.rawResponse = response
+  parseResults.callError = requestError
+  parseResults.AnalyzeTLS()
 
-  if err == nil && (resp.StatusCode == 301 || resp.StatusCode == 302) {
-    redirectHeaders := resp.Header
-    if _, available := redirectHeaders["Location"]; available {
-      parsedLocation, _ := url.Parse(redirectHeaders["Location"][0])
-      if parsedLocation.Scheme == "https" {
-        req.URL = parsedLocation
-        resp, err = client.Do(req)
-        fmt.Println("<< redirected >>")
-      } else {
-        fmt.Println("<< does not have https equivalent >>")
-      }
-    }
-  } else {
-    fmt.Println("<< no redirection >>")
-  }*/
-
-  ////////////////////
-  /*fmt.Println("received the following> ")
-  fmt.Printf("%s\nstatus: %d\ncontent length received: %d\n\n", resp.Proto, resp.StatusCode, resp.ContentLength)
-  fmt.Println("headers:")
-  for header, value := range resp.Header {
-    fmt.Printf("\t%s: %s\n", header, value)
-  }
-
-  if resp.TLS != nil {
-    //talk about state of tls connection
-    fmt.Printf("site connection encrypted, using:\nversion:\t%s\ncipher:\t\t%s\n", tlsVersionsByCode[resp.TLS.Version], cipherSuitesByCode[resp.TLS.CipherSuite])
-    fmt.Printf("handshake completed: %t\n", resp.TLS.HandshakeComplete)
-    fmt.Printf("connection presented %d certificate(s)\n", len(resp.TLS.PeerCertificates))
-    //look at certs presented
-    for i:=0; i<len(resp.TLS.PeerCertificates); i++ {
-      currentCert := resp.TLS.PeerCertificates[i]
-      //certs are displayed (numbered) with a SHA1 fingerprint then some datapoints
-      fmt.Printf("%d) - % x\n\tissuer: %+v\n\tsubject: %+v\n", i+1, sha1.Sum(currentCert.Raw), currentCert.Issuer, currentCert.Subject)
-      fmt.Printf("\texpires on %s\n", currentCert.NotAfter.String())
-      //subject alternative names
-      if len(currentCert.DNSNames) > 0 {
-        fmt.Printf("\tSANs-DNS:\n\t\t%v\n", currentCert.DNSNames)
-      }
-      if len(currentCert.IPAddresses) > 0 {
-        fmt.Printf("\tSANs-IPs:\n\t\t%v\n", currentCert.IPAddresses)
-      }
-      //TODO: maybe a container to hold each cert then do something if it's a CA?
-      fmt.Printf("\tCA: %t\n\n", currentCert.IsCA)
-    }
-  } else {
-    fmt.Println("site connection is unencrypted")
-  }*/
+  return parseResults.Serialize()
 }
---
+++ parsesitecontent_test.go
@@ -0,0 +1,89 @@
+package main
+import (
+  "net/http"
+  "strings"
+  "testing"
+)
+
+type mockRedirect struct {
+  response *http.Response
+  site string
+}
+
+func CreateTest(url string, httpStatus int, locationIsHttps bool) *mockRedirect {
+  testCase := new(mockRedirect)
+  fakeRequest, _ := http.NewRequest("GET", url, nil)
+
+  testCase.site = url
+  testCase.response = &http.Response{
+    Header: make(http.Header),
+    Request: fakeRequest,
+    StatusCode: httpStatus,
+  }
+
+  if locationIsHttps {
+    testCase.response.Header.Add("Location", strings.Replace(testCase.site, "http", "https", 1))
+  }
+
+  return testCase
+}
+
+func TestCheckForRedirection(t *testing.T) {
+  //test cases
+  //  1: redirects to https url
+  //  2: redirects to non-secure url
+  //  2: no redirection and site requested is unchanged
+  var expectedSite string = "http://isthisthesame.net"
+  tc1 := CreateTest("http://somedomain.com", 301, true)
+  tc2 := CreateTest("http://somedomain.com", 302, false)
+  tc3 := CreateTest(expectedSite, 200, false)
+
+  found, locIsHttp, loc := CheckForRedirection(tc1.response)
+  if !found || !locIsHttp || loc[0:8] != "https://" {
+    t.Errorf("tc1 - expected to find a redirect to https url> redirected: %t, headerLocation: %s", found, loc)
+  }
+
+  found, locIsHttp, loc = CheckForRedirection(tc2.response)
+  if !found || locIsHttp || loc[0:7] != "http://" {
+    t.Errorf("tc2 - expected to find a redirect to http url> redirected: %t, headerLocation: %s", found, loc)
+  }
+
+  found, locIsHttp, loc = CheckForRedirection(tc3.response)
+  if found || locIsHttp || loc != expectedSite {
+    t.Errorf("tc3 - expected not to find a redirect, site should remain the same> redirected: %t, headerLocation: %s", found, loc)
+  }
+}
+
+func TestFormatUrl(t *testing.T) {
+  //test cases
+  //  1: returns url with http scheme
+  //  2: returns url with https scheme
+  //  3: returns empty string on url.Parse() error
+  //  4: returns empty string on zero length host argument
+  //  5: returns url with http scheme if secure argument is false
+  tc1 := FormatUrl("host.com", false)
+  tc2 := FormatUrl("host.com", true)
+  tc3 := FormatUrl("://host.com", false)
+  tc4 := FormatUrl("", false)
+  tc5 := FormatUrl("https://host.com", false)
+
+  if len(tc1) == 0 || tc1[0:7] != "http://" {
+    t.Errorf("tc1 - expected \"http://host.com\", recieved: \"%s\"", tc1)
+  }
+
+  if len(tc2) == 0 || tc2[0:8] != "https://" {
+    t.Errorf("tc2 - expected \"http://host.com\", recieved: \"%s\"", tc2)
+  }
+
+  if len(tc3) != 0 {
+    t.Error("tc3 - expected empty string on url.Parse() error")
+  }
+
+  if len(tc4) != 0 {
+    t.Error("tc4 - expected empty string on empty host argument")
+  }
+
+  if len(tc5) == 0 || tc5[0:7] != "http://" {
+    t.Errorf("tc5 - expected \"http\" scheme, received: \"%s\"", tc5)
+  }
+}
